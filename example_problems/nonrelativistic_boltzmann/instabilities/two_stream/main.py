b5028aa7ba75faca42b2c28aab114af1a829c9b6
Ben Ryan <bryan10@illinois.edu>
2018-08-14 11:46:51 -0600

    Working

--- example_problems/nonrelativistic_boltzmann/instabilities/two_stream/main.py
+++ example_problems/nonrelativistic_boltzmann/instabilities/two_stream/main.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('agg')
 import pylab as pl
-import h5py
+#import h5py
 
 from bolt.lib.physical_system import physical_system
 from bolt.lib.nonlinear.nonlinear_solver import nonlinear_solver
@@ -62,7 +62,7 @@ system = physical_system(domain,
 # Declaring a linear system object which will evolve the defined physical system:
 nls = nonlinear_solver(system)
 N_g = nls.N_ghost
-ls  = linear_solver(system)
+#ls  = linear_solver(system)
 
 # Time parameters:
 dt = params.N_cfl * min(nls.dq1, nls.dq2) \
@@ -71,70 +71,70 @@ dt = params.N_cfl * min(nls.dq1, nls.dq2) \
 time_array = np.arange(0, params.t_final + dt, dt)
 
 # Initializing Arrays used in storing the data:
-E_data_ls  = np.zeros_like(time_array)
-E_data_nls = np.zeros_like(time_array)
+#E_data_ls  = np.zeros_like(time_array)
+#E_data_nls = np.zeros_like(time_array)
 
-ke_data_ls  = np.zeros_like(time_array)
-ke_data_nls = np.zeros_like(time_array)
+#ke_data_ls  = np.zeros_like(time_array)
+#ke_data_nls = np.zeros_like(time_array)
 
 for time_index, t0 in enumerate(time_array):
     
-    if(time_index%10 == 0):
-        nls.dump_distribution_function('dump_f/%04d'%time_index)
+    #if(time_index%10 == 0):
+    #    nls.dump_distribution_function('dump_f/%04d'%time_index)
 
     print('Computing For Time =', t0)
 
-    E_data_nls[time_index] = af.max(nls.fields_solver.cell_centered_EM_fields[:, :, N_g:-N_g, N_g:-N_g])
-    E1_ls                  = af.real(0.5 * (ls.N_q1 * ls.N_q2) 
-                                         * ifft2(ls.fields_solver.E1_hat)
-                                    )
+    #E_data_nls[time_index] = af.max(nls.fields_solver.cell_centered_EM_fields[:, :, N_g:-N_g, N_g:-N_g])
+    #E1_ls                  = af.real(0.5 * (ls.N_q1 * ls.N_q2) 
+    #                                     * ifft2(ls.fields_solver.E1_hat)
+    #                                )
 
-    E_data_ls[time_index]  = af.max(E1_ls)
+    #E_data_ls[time_index]  = af.max(E1_ls)
 
-    ke_data_nls[time_index] = af.mean(nls.compute_moments('mom_v1_bulk')**2 / nls.compute_moments('density'))
-    ke_data_ls[time_index]  = af.mean(ls.compute_moments('mom_v1_bulk')**2 / ls.compute_moments('density'))
+    #ke_data_nls[time_index] = af.mean(nls.compute_moments('mom_v1_bulk')**2 / nls.compute_moments('density'))
+    #ke_data_ls[time_index]  = af.mean(ls.compute_moments('mom_v1_bulk')**2 / ls.compute_moments('density'))
 
     nls.strang_timestep(dt)
     # ls.RK4_timestep(dt)
 
-import h5py
-h5f = h5py.File('data.h5', 'w')
-h5f.create_dataset('electrical_field_ls', data = E_data_ls)
-h5f.create_dataset('electrical_field_nls', data = E_data_nls)
-h5f.create_dataset('kinetic_energy_ls', data = ke_data_ls)
-h5f.create_dataset('kinetic_energy_nls', data = ke_data_nls)
-h5f.create_dataset('time', data = time_array)
-h5f.close()
+#import h5py
+#h5f = h5py.File('data.h5', 'w')
+#h5f.create_dataset('electrical_field_ls', data = E_data_ls)
+#h5f.create_dataset('electrical_field_nls', data = E_data_nls)
+#h5f.create_dataset('kinetic_energy_ls', data = ke_data_ls)
+#h5f.create_dataset('kinetic_energy_nls', data = ke_data_nls)
+#h5f.create_dataset('time', data = time_array)
+#h5f.close()
 
-pl.plot(time_array, E_data_nls, label='Nonlinear Solver')
-#pl.plot(time_array, E_data_ls, '--', color = 'black', label = 'Linear Solver')
-pl.ylabel(r'MAX($|E|$)')
-pl.xlabel('Time')
-#pl.legend()
-pl.savefig('linearplot.png')
-pl.clf()
+#pl.plot(time_array, E_data_nls, label='Nonlinear Solver')
+##pl.plot(time_array, E_data_ls, '--', color = 'black', label = 'Linear Solver')
+#pl.ylabel(r'MAX($|E|$)')
+#pl.xlabel('Time')
+##pl.legend()
+#pl.savefig('linearplot.png')
+#pl.clf()
 
-pl.semilogy(time_array, E_data_nls, label='Nonlinear Solver')
-#pl.semilogy(time_array, E_data_ls, '--', color = 'black', label = 'Linear Solver')
-pl.ylabel(r'MAX($|E|$)')
-pl.xlabel('Time')
-#pl.legend()
-pl.savefig('semilogyplot.png')
-pl.clf()
+#pl.semilogy(time_array, E_data_nls, label='Nonlinear Solver')
+##pl.semilogy(time_array, E_data_ls, '--', color = 'black', label = 'Linear Solver')
+#pl.ylabel(r'MAX($|E|$)')
+#pl.xlabel('Time')
+##pl.legend()
+#pl.savefig('semilogyplot.png')
+#pl.clf()
 
-pl.plot(time_array, ke_data_nls, label='Nonlinear Solver')
-#pl.plot(time_array, ke_data_ls, '--', color = 'black', label = 'Linear Solver')
-pl.ylim(-0.02, 0.02)
-pl.ylabel(r'MEAN(KE)')
-pl.xlabel('Time')
-#pl.legend()
-pl.savefig('ke_linearplot.png')
-pl.clf()
+#pl.plot(time_array, ke_data_nls, label='Nonlinear Solver')
+##pl.plot(time_array, ke_data_ls, '--', color = 'black', label = 'Linear Solver')
+#pl.ylim(-0.02, 0.02)
+#pl.ylabel(r'MEAN(KE)')
+#pl.xlabel('Time')
+##pl.legend()
+#pl.savefig('ke_linearplot.png')
+#pl.clf()
 
-pl.semilogy(time_array, ke_data_nls, label='Nonlinear Solver')
-#pl.semilogy(time_array, ke_data_ls, '--', color = 'black', label = 'Linear Solver')
-pl.ylabel(r'MEAN(KE)')
-pl.xlabel('Time')
-#pl.legend()
-pl.savefig('ke_semilogyplot.png')
-pl.clf()
+#pl.semilogy(time_array, ke_data_nls, label='Nonlinear Solver')
+##pl.semilogy(time_array, ke_data_ls, '--', color = 'black', label = 'Linear Solver')
+#pl.ylabel(r'MEAN(KE)')
+#pl.xlabel('Time')
+##pl.legend()
+#pl.savefig('ke_semilogyplot.png')
+#pl.clf()
