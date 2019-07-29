cc2c406aaa6c60162125f3e005e7344a1b069d1d
Ben Ryan <bryan10@illinois.edu>
2019-07-29 10:46:06 -0600

    New approach to most recent restarts in main.py

--- example_problems/nonrelativistic_boltzmann/instabilities/collisionless_shock/1D/main.py
+++ example_problems/nonrelativistic_boltzmann/instabilities/collisionless_shock/1D/main.py
@@ -8,6 +8,7 @@ MPI.WTIME_IS_GLOBAL=True
 from bolt.lib.physical_system import physical_system
 from bolt.lib.nonlinear.nonlinear_solver import nonlinear_solver
 from bolt.lib.utils.parallel_reduction_ops import global_min, global_mean
+from bolt.lib.utils.restart_latest import latest_output, format_time
 
 import domain
 import boundary_conditions
@@ -41,16 +42,27 @@ dt_fdtd = params.N_cfl * min(nls.dq1, nls.dq2) \
 
 dt = min(dt_fvm, dt_fdtd)
 
-if(params.t_restart == 0):
-    time_elapsed = 0
-    nls.dump_distribution_function('dump_f/t=0.000000')
-    nls.dump_moments('dump_moments/t=0.000000')
-    nls.dump_EM_fields('dump_fields/t=0.000000')
-
-else:
-    time_elapsed = params.t_restart
-    nls.load_distribution_function('dump_f/t=' + '%.6f'%time_elapsed)
-    nls.load_EM_fields('dump_fields/t=' + '%.6f'%time_elapsed)
+using_latest_restart = False
+if(params.latest_restart == True):                                               
+    latest_f, latest_fields, time_elapsed = latest_output('')                    
+    print(time_elapsed)                                                          
+    if(latest_f is not None and latest_fields is not None and                    
+       time_elapsed is not None):                                                
+      nls.load_distribution_function(latest_f)                                   
+      nls.load_EM_fields(latest_fields)                                          
+      using_latest_restart = True                                                
+if using_latest_restart == False:                                                
+    if(params.t_restart == 0 or params.latest_restart == True):                  
+        time_elapsed = 0                                                         
+        formatted_time = format_time(time_elapsed)                               
+        nls.dump_distribution_function('dump_f/t=' + formatted_time)             
+        nls.dump_moments('dump_moments/t=' + formatted_time)                     
+        nls.dump_EM_fields('dump_fields/t=' + formatted_time)                    
+    else:                                                                        
+        time_elapsed = params.t_restart                                          
+        formatted_time = format_time(time_elapsed)                               
+        nls.load_distribution_function('dump_f/t=' + formatted_time)             
+        nls.load_EM_fields('dump_fields/t=' + formatted_time)
 
 # Checking that the file writing intervals are greater than dt:
 assert(params.dt_dump_f > dt)
@@ -75,6 +87,7 @@ PETSc.Sys.Print('Ions     :', global_mean(E[:, 1, :, :]))
 PETSc.Sys.Print('\n')
 
 timing_data = []
+nstep = 0
 while(abs(time_elapsed - params.t_final) > 1e-12):
 
     tic = MPI.Wtime()
@@ -91,15 +104,17 @@ while(abs(time_elapsed - params.t_final) > 1e-12):
         if((delta_dt-dt)<1e-5):
             nls.strang_timestep(delta_dt)
             time_elapsed += delta_dt
-            nls.dump_moments('dump_moments/t=' + '%.6f'%time_elapsed)
-            nls.dump_EM_fields('dump_fields/t=' + '%.6f'%time_elapsed)
+            formatted_time = format_time(time_elapsed)
+            nls.dump_moments('dump_moments/t=' + formatted_time)
+            nls.dump_EM_fields('dump_fields/t=' + formatted_time)
 
     if(math.modf(time_elapsed/params.dt_dump_f)[0] < 1e-12):
         nls.dump_distribution_function('dump_f/t=' + '%.6f'%time_elapsed)
 
-    PETSc.Sys.Print('Time =', format(time_elapsed / params.t0, '.4f'),
-                    't0, dt = ', format(dt / params.t0, '.4f'),
-                    't0, time taken = ', format(toc - tic, '.4f'), 'secs'
-                   )
+    if (nstep % 10 == 0):
+        PETSc.Sys.Print('Time =', format(time_elapsed / params.t0, '.4f'),
+                        't0, dt = ', format(dt / params.t0, '.4f'),
+                        't0, time taken = ', format(toc - tic, '.4f'), 'secs'
+                       )
 
     timing_data.append(toc - tic)
